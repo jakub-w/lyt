3d0267dddaa812c6b8b6ab7d7f98927291a8a4e0
jakub-w <cubex7@gmail.com>
2019-03-25 19:40:15 +0100

    Added the start of a documentation for Operation class.

--- Operation.h
+++ Operation.h
@@ -9,6 +9,24 @@ namespace yt {
   const std::string API_AUTHORITY = "www.googleapis.com";
   const std::string API_PATH = "/youtube/v3/";
 
+/**
+ * \brief Operation is an abstract base for operation classes
+ *
+ * It's only purpose is to add a YouTube api key to parameters_ and ensure
+ * that its children define `GetActionName()`.
+ *
+ * ### Inheriting Operation
+ *
+ * Classes that inherit Operation define queries to the YouTube API and
+ * implement calls for specific resources from it.
+ *
+ * Every Operation's child should define `Perform()` function
+ * that returns `std::unique_ptr` to the concrete Response object that
+ * corresponds to it.
+ * (E.g. for SearchOperation: `std::unique_ptr<SearchResponse> Perform()`).
+ *
+ * To be continued...
+ */
 class Operation {
  protected:
   std::unordered_map<std::string, std::string> parameters_;
